28581875d50cde18a902847265b1f26b2d2f1e3b
DongManhDung <dongdung0407@gmail.com>
2026-01-14 10:56:43 +0700

    feat: Working on alpha-landing-page test. Init vite + react project. Add tailwind css library

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="min-h-screen flex items-center justify-center bg-yellow-500">
+        <h1 className="text-white text-4xl font-bold">
+          Tailwind v3 is working 🎉
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
